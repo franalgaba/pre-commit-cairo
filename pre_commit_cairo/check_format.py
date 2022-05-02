58bd53752918ae98ba210f3a3cee959a26c94d09
franalgaba <f.algaba@outlook.es>
2022-05-02 17:38:32 +0200

    simplify output

--- pre_commit_cairo/check_format.py
+++ pre_commit_cairo/check_format.py
@@ -11,36 +11,12 @@ from rich.console import Console
 console = Console()
 
 
-def _format_output(reformatted_count, unchanged_count, failed_count):
-    console.print("All done! :sparkles: :cake: :sparkles:")
-    final_output = []
-    if reformatted_count > 0:
-        if reformatted_count > 1:
-            final_output.append(f"{reformatted_count} files reformatted")
-        else:
-            final_output.append(f"{reformatted_count} file reformatted")
-    if unchanged_count > 0:
-        if unchanged_count > 1:
-            final_output.append(f"{reformatted_count} files left unchanged")
-        else:
-            final_output.append(f"{reformatted_count} file left unchanged")
-    if failed_count > 0:
-        if failed_count > 1:
-            final_output.append(f"{failed_count} files failed to reformat")
-        else:
-            final_output.append(f"{failed_count} file failed to reformat")
-    console.print(", ".join(final_output))
-
-
 def main(argv: Sequence[str] | None = None) -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument("filenames", nargs="*")
     args = parser.parse_args(argv)
 
     retval = 0
-    reformatted_count = 0
-    unchanged_count = 0
-    failed_count = 0
     for filename in args.filenames:
         if filename.endswith(".cairo"):
             prev_timestamp = os.path.getmtime(filename)
@@ -53,22 +29,15 @@ def main(argv: Sequence[str] | None = None) -> int:
                     check=True,
                 )
             except Exception:
-                failed_count += 1
                 console.print(f"[red]error: cannot format {filename}[/red]")
                 retval = 1
             else:
                 if prev_timestamp != os.path.getmtime(filename):
-                    reformatted_count += 1
                     console.print(f"reformatted contract: {filename}")
                     retval = 1
-                else:
-                    unchanged_count += 1
 
-    return retval, reformatted_count, unchanged_count, failed_count
+    return retval
 
 
 if __name__ == "__main__":
-    retval, reformatted_count, unchanged_count, failed_count = main()
-    _format_output(reformatted_count, unchanged_count, failed_count)
-
-    raise SystemExit(retval)
+    raise SystemExit(main())
